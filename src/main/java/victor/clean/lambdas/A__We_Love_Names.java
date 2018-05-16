cb7b754d2e9823dbbbed3ba47f4d251f297890d8
Victor Rentea <victorrentea@gmail.com>
2018-05-16 18:38:40 +0300

    Constructor :: doamne!!

--- src/main/java/victor/clean/lambdas/A__We_Love_Names.java
+++ src/main/java/victor/clean/lambdas/A__We_Love_Names.java
@@ -15,22 +15,17 @@ class UserFacade {
 	private UserRepo userRepo;
 	
 	public List<UserDto> getAllUsers() {
-		List<User> users = userRepo.findAll();
-		List<UserDto> dtos = new ArrayList<>();
-		for (User user : users) {
-			UserDto dto = new UserDto();
-			dto.setUsername(user.getUsername());
-			dto.setFullName(user.getFirstName() + " " + user.getLastName().toUpperCase());
-			dto.setActive(user.getDeactivationDate() == null);
-			dtos.add(dto);
-		}
-		return dtos;
+		return userRepo.findAll().stream().map(UserDto::new).collect(toList());
+	}
+
+	private UserDto toDto(User user) {
+		UserDto dto = new UserDto(user);
+//		dto.setUsername(user.getUsername());
+//		dto.setFullName(user.getFirstName() + " " + user.getLastName().toUpperCase());
+//		dto.setActive(user.getDeactivationDate() == null);
+		return dto;
 	}
 }
-
-
-
-
 
 
 
@@ -52,6 +47,11 @@ class User {
 
 @Data
 class UserDto {
+	public UserDto(User user) {
+		this.setUsername(user.getUsername());
+		this.setFullName(user.getFirstName() + " " + user.getLastName().toUpperCase());
+		this.setActive(user.getDeactivationDate() == null);
+	}
 	private String fullName;
 	private String username;
 	private boolean active;
